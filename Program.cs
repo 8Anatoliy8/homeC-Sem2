22f4222f10932f1c34d284c152314164f28d20eb
Anatoliy Bogdevich <tolya.bogdevich@mail.ru>
2023-12-27 22:30:29 +0300

    complete Задача2

--- Program.cs
+++ Program.cs
@@ -1,12 +1,37 @@
-﻿// Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+﻿// // Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
 
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-if (number % 7 == 0 && number % 27 == 0)
-{
-    Console.WriteLine($"Число {number} кратно 7 и 27");
-}
-else
-{
-    Console.WriteLine($"Число {number} некратно 7 и 27");
-}
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number % 7 == 0 && number % 27 == 0)
+// {
+//     Console.WriteLine($"Число {number} кратно 7 и 27");
+// }
+// else
+// {
+//     Console.WriteLine($"Число {number} некратно 7 и 27");
+// }
+
+// // Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0
+// // и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+
+
+// Console.Write("Введите число X: ");
+// int X = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число Y: ");
+// int Y = Convert.ToInt32(Console.ReadLine());
+// if (X>0 && Y>0)
+// {
+//     Console.WriteLine($"Точка находится в I координатной четверти");
+// }
+// if (X<0 && Y>0)
+// {
+//     Console.WriteLine($"Точка находится во II координатной четверти");
+// }
+// if (X<0 && Y<0)
+// {
+//     Console.WriteLine($"Точка находится в III координатной четверти");
+// }
+// if (X>0 && Y<0)
+// {
+//     Console.WriteLine($"Точка находится в IV координатной четверти");
+// }
